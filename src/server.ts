01eae4a687e3d6c0f78a72486de9e35a83433906
Adrian Bielenik <adrian.bielenik@gmail.com>
2025-10-04 16:27:24 +0200

    added registration

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import * as express from 'express';
 import type {Request, Response} from 'express';
+import authRouter from './routes/auth.js';
 
 const app = express.default();
 const PORT = process.env.PORT || 3000;
@@ -7,10 +8,11 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 app.get('/', (req:Request , res:Response) =>{
-    res.send('Shelf api: konfiguracja ESM/TS zakonczona');
+    res.send('Shelf api: serwer hula smiga');
 });
+
+app.use('/api/auth', authRouter);
 
 app.listen(PORT, ()=>{
     console.log(`[server]: serwer dziala na porcie ${PORT}`);
-
 })
